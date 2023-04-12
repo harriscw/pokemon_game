bbf6d6573be6f3983434d6758774a036614ddc06
harriscw <harriscw@ucla.edu>
2023-04-11 18:25:51 -0700

    change region

--- app.R
+++ app.R
@@ -162,25 +162,6 @@ server <- function(input, output) {
               uiOutput("pokeball_display"),
               width=3,offset = 0, style='padding: 0px 0px;margin:-2em'
             )
-          ),
-          hr(),
-          fluidRow(
-            selectInput("region", label = h3("Select Region"), 
-                        choices = list(
-                          # "Dragon" = "dragon",
-                          # "Hawaii" = "hawaii",
-                          "Water" = "water",
-                          # "Sky" = "sky",
-                          # "Grass" = "grass",
-                          "Egypt" = "egypt",
-                          # "Poison" = "poison",
-                          # "Fire" = "fire",
-                          "Rock" = "rock",
-                          "Ice" = "ice",
-                          "Random"="random"
-                        ), 
-                        selected = "water"),
-            img(src = "map.jpg",height="60%", width="60%")
           )
         )
       )
@@ -200,21 +181,29 @@ server <- function(input, output) {
       
       div(
       h3(paste("You have chosen:",input$trainer,input$buddy,input$bag,input$pokeball)),
-      h3(paste("You are in the ",the_region(),"region.")),
+      hr(),
+      fluidRow(
+        selectInput("region", label = h3("Select Region"), 
+                    choices = list(
+                      # "Dragon" = "dragon",
+                      # "Hawaii" = "hawaii",
+                      "Water" = "water",
+                      # "Sky" = "sky",
+                      # "Grass" = "grass",
+                      "Egypt" = "egypt",
+                      # "Poison" = "poison",
+                      # "Fire" = "fire",
+                      "Rock" = "rock",
+                      "Ice" = "ice",
+                      "Random"="random"
+                    ), 
+                    selected = "water"),
+        img(src = "map.jpg",height="60%", width="60%")
+      ),
       uiOutput("pokemon_encounter"),
       actionButton("go","Go"),
-      verbatimTextOutput("pokedex_display"),
-      # selectInput("region_chg",label="Change Region",choices=list(
-      #   "Dragon" = "dragon",
-      #   "Hawaii" = "hawaii",
-      #   "Water" = "water",
-      #   "Sky" = "sky",
-      #   "Grass" = "grass",
-      #   "Egypt" = "egypt",
-      #   "Poison" = "poison",
-      #   "Fire" = "fire",
-      #   "Rock" = "rock"
-      # ),selected=the_region())
+      verbatimTextOutput("pokedex_display")
+      
       )
       
       
@@ -222,29 +211,20 @@ server <- function(input, output) {
     
   })
   
-  the_region=reactive({
-    
-    # req(input$play)
-    
-    # if(input$play!=1){
-      input$region
-    # }else{
-    #   input$region_chg
-    # }
-  })
-  
-  # theregion=reactive({input$region})
+ 
   pokedex=reactiveValues(thelist=list())
   legendaries <- gsub("\\.jpg","",list.files(paste0("www/pokemon/legendary")))
   
   themons=reactive({
     
-    themons=gsub("\\.jpg","",list.files(paste0("www/pokemon/",the_region())))
+    themons=gsub("\\.jpg","",list.files(paste0("www/pokemon/",input$region)))
   # themons=themons[themons!="oclti"]
     themons
   })
   
   observeEvent(input$go,{
+    
+    current_region=input$region
     
     if(input$go==1){
       thepokemon<<-sample(themons(),2) #get an initial sample
@@ -260,8 +240,9 @@ server <- function(input, output) {
     output$pokemon_encounter = renderUI({
       
       div(
+        h3(paste("You are in the ",current_region,"region.")),
         h3(paste0("Wow! Its ",thepokemon[2],"! ",if(thepokemon[2] %in% legendaries){"WOW!!!!!  It's LEGENDARY!!!!!!!!!!"}else{""})),
-        img(src = paste0("pokemon/",if(thepokemon[2] %in% legendaries){"legendary"}else{the_region()},"/",thepokemon[2],".jpg"),height="10%", width="10%"),
+        img(src = paste0("pokemon/",if(thepokemon[2] %in% legendaries){"legendary"}else{current_region},"/",thepokemon[2],".jpg"),height="10%", width="10%"),
         selectInput("what_to_do", label = "What do you want to do?", 
                     choices = list("Catch It" = "catch", "Run" = "run"), 
                     selected = "catch"),
